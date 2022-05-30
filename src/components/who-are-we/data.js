e84d08d537bc49c8c53c33cb081aa32935d8a9ff
Krishna Dhingra <dkrishna1608@gmail.com>
2022-05-30 12:52:27 +0530

    Updated the icons

--- src/components/who-are-we/data.js
+++ src/components/who-are-we/data.js
@@ -1,28 +1,29 @@
-import CameraImage from '../../utilities/who-are-we-camera.svg'
-import Camera2Image from '../../utilities/who-are-we-camera-2.svg'
-import CoupleImage from '../../utilities/who-are-we-couple.svg'
+import WhoAreWeCandid from '../../utilities/who-are-we-candid.svg'
+import WhoAreWeTraditional from '../../utilities/who-are-we-traditional.svg'
+import WhoAreWeWedding from '../../utilities/who-are-we-wedding.svg'
+import WhoAreWeDocumentary from '../../utilities/who-are-we-documentary.svg'
 
 export const data = [
   {
-    image: CameraImage,
+    image: WhoAreWeCandid,
     subheading: 'Candid / Artistic photography',
     text:
       'Candid photographs are more artistic and way more natural. In Candid photography, the photographer is keen on capturing real emotions. And the moments are precisely frozen naturally without artificially posing. Candid photographs can make you feel the same vibe you had on your big day every time you see those photographs.',
   },
   {
-    image: CoupleImage,
+    image: WhoAreWeWedding,
     subheading: 'Wedding Films',
     text:
       "Every bride and groom wants to replicate a celebrity on their big day. We at OOAK will help to fulfill the couple's desire by capturing their wedding sequence in the most creative and cinematic way by using all the cinematic techniques to make them feel as if they are watching a romantic movie.",
   },
   {
-    image: Camera2Image,
+    image: WhoAreWeTraditional,
     subheading: 'Conventional Photography',
     text:
       'Traditional photography covers the stage photos covering the crowd present, capturing every event and ritual. In traditional photography, events are documented or filmed in the way they are. The sequence of the wedding is captured and you will know what to expect in your wedding album.',
   },
   {
-    image: Camera2Image,
+    image: WhoAreWeDocumentary,
     subheading: 'Documentary Wedding',
     text:
       "Weddings are rituals narrating the family's tradition and pride flowing through generations and making it unique in every single way. A documentary wedding captures things as they happen, which means being truthful to the scene in front of you and the moment you are capturing.",
