4b28ddfe7c52eaa42dffa00804e863e31483fc6b
Krishna Dhingra <dkrishna1608@gmail.com>
2022-05-31 18:40:58 +0530

    Resolved the google maps error

--- src/components/get-in-touch-map/get-in-touch-map.js
+++ src/components/get-in-touch-map/get-in-touch-map.js
@@ -1,47 +1,45 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
 
-const containerStyle = {
-  width: '100vw',
-  height: '400px',
-}
-
-const center = {
-  lat: 28.64365478930003,
-  lng: 77.11235316987492,
-}
-
 function GetInTouchMap() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  })
+  // const { isLoaded } = useJsApiLoader({
+  //   id: 'google-map-script',
+  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  // })
 
-  const [map, setMap] = React.useState(null)
+  // const [map, setMap] = React.useState(null)
 
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center)
-    map.fitBounds(bounds)
-    setMap(map)
-  }, [])
+  // const onLoad = React.useCallback(function callback(map) {
+  //   const bounds = new window.google.maps.LatLngBounds(center)
+  //   map.fitBounds(bounds)
+  //   setMap(map)
+  // }, [])
 
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
+  // const onUnmount = React.useCallback(function callback(map) {
+  //   setMap(null)
+  // }, [])
 
-  return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={10}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      {/* Child components, such as markers, info windows, etc. */}
-      <></>
-    </GoogleMap>
-  ) : (
-    <></>
+  // return isLoaded ? (
+  //   <GoogleMap
+  //     mapContainerStyle={containerStyle}
+  //     center={center}
+  //     zoom={10}
+  //     onLoad={onLoad}
+  //     onUnmount={onUnmount}
+  //   >
+  //     {/* Child components, such as markers, info windows, etc. */}
+  //     <></>
+  //   </GoogleMap>
+  // ) : (
+  //   <></>
+  // )
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3325395304414!2d-122.01116148467422!3d37.33463524513264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb59127ce078f%3A0x18e1c3ce7becf1b!2sApple%20Park!5e0!3m2!1sen!2sin!4v1637309850935!5m2!1sen!2sin"
+      style={{ width: '100vw', height: '400px' }}
+      allowFullScreen=""
+      loading="lazy"
+    ></iframe>
   )
 }
 export default GetInTouchMap
